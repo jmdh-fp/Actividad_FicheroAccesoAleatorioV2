38097e0cec8b50a94612dd81cca6a20a596d93f7
jm-upmd <43405929+jm-upmd@users.noreply.github.com>
2023-11-01 13:38:02 +0100

    Commit

--- src/main/java/com/example/org/MainPalCompleta.java
+++ src/main/java/com/example/org/MainPalCompleta.java
@@ -1,26 +1,30 @@
 
 package com.example.org;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  @author Jose M.
- Pide una secuencia de caracteres y un fichero de texto por consola.
- Sustituye la secuencia de caracteres por su equivalente en mayusculas.
+ Pide una palabra  y un fichero de texto por consola.
+ Sustituye la palabra por su equivalente en mayusculas.
  Realiza acceso aleatorio al fichero.
  */
-public class Main {
-
+public class MainPalCompleta {
+    static long numSustituciones = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String fichero, palabra, linea;
         int index;
 
         long numEscrituras = 0;
-        long numSustituciones = 0;
 
-        StringBuilder sb = new StringBuilder();
+
 
         do {
             System.out.println("Nombre del fichero: ");
@@ -32,8 +36,6 @@ public class Main {
 
         // Constantes con palabra en mayúscula y su longitud
         final String PAL_MAY = palabra.toUpperCase();
-        final int LONG_PALABRA = palabra.length();
-
         long posFichero = 0;
 
         long tiempo = System.currentTimeMillis();
@@ -41,21 +43,12 @@ public class Main {
         try (RandomAccessFile archivo = new RandomAccessFile(fichero, "rw")) {
             archivo.seek(0);
             while ((linea = archivo.readLine()) != null) {
-                sb.setLength(0); // Limpia el sb
                 index = linea.indexOf(palabra);
                 if (index != -1) {  // Si hay palabra
-                    numSustituciones++;
                     archivo.seek(posFichero);//posición a partir de la cual escribir en fichero
-
-                    // Reemplazos en un sb es mucho más eficiente que con String, por ej. con linea.replaceall(...)
-                    sb.append(linea);
-                    do {
-                        sb.replace(index, index + LONG_PALABRA, PAL_MAY);
-                        numSustituciones++;
-                    }  while ((index = sb.indexOf(palabra, index + LONG_PALABRA)) != -1);
-
+                    linea = reemplazaPalabraMay(linea,palabra,PAL_MAY);
                     // Graba el string en posición posFichero
-                    archivo.writeBytes(sb.toString());
+                    archivo.writeBytes(linea);
                     numEscrituras++;
                 } // if
 
@@ -70,5 +63,14 @@ public class Main {
         System.out.println("Tiempo: " + (System.currentTimeMillis() - tiempo));
         System.out.println("Número de escrituras: " + numEscrituras);
         System.out.println("Número de sustituciones: " + numSustituciones);
+    }
+
+    static private String reemplazaPalabraMay(String linea, String palOrig, String palSust){
+        StringBuilder sb = new StringBuilder(linea);
+        Pattern p = Pattern.compile("\\b"+palOrig+"\\b");  // Crea patrón de búsqueda
+        Matcher m = p.matcher(sb);  // Resultado busqueda sobre el sb
+        numSustituciones+= m.results().count(); // Número de coincidencias
+        return m.replaceAll(palSust);  // Sustitución de las coincidencias en el sb por palSust
+
     }
 }
